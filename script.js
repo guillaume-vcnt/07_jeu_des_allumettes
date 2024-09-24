d1991c17bbc63297db07190ef668c9c3a581a54e
Guillaume <guillaume.vincent1138@gmail.com>
2024-09-24 16:48:14 +0200

    Étape 4

---
+++ script.js
@@ -0,0 +1,30 @@
+//07_jeu_des_allumettes
+
+alert("Welcome to the match game!");
+
+let UsernameIsDefined = false;
+
+while (!UsernameIsDefined) {
+  let userName = prompt("What is your name ?");                  
+  if (userName.trim() !== "" && isNaN(parseInt(userName))) {
+    alert("Hello, " + userName + " let's play");               
+    UsernameIsDefined = true;
+  } else {
+    alert("Enter a valid name!");
+  }
+}
+
+let RemoveMatchIsDefined = false;
+
+while (!RemoveMatchIsDefined) {
+  let removeMatch = prompt("How many match you want to remove?");
+  if (removeMatch >= 1 && removeMatch <= 6 && removeMatch.trim() !== "")
+    RemoveMatchIsDefined = true;
+  else {
+    alert("Enter a number between 0 to 7 !");
+  }
+}
+
+
+
+
